a00d68938552448a8927c10e9674712a8ed0a1d1
Eusebio Simango <eusebiosimango14@gmail.com>
2022-07-13 21:54:17 +0200

    change advice dinamicaly onClick

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,6 @@
 const adviceMessageEl = window.document.getElementById("adviceMessage")
 const adviceIdEl = window.document.getElementById("adviceId")
+const nextAdviceEl = window.document.getElementById("nextAdvice")
 
 const advice = {
   id: "001",
@@ -8,3 +9,15 @@ const advice = {
 
 adviceIdEl.innerText = advice.id
 adviceMessageEl.innerHTML = `"${advice.text}"`
+
+const sortAdvice = () => {
+  const advice = {
+    id: `002`,
+    text: "It is easy to sit up and take notice, what's difficult is getting up and taking action."
+  }
+
+  adviceIdEl.innerText = advice.id
+  adviceMessageEl.innerHTML = `"${advice.text}"`
+}
+
+nextAdviceEl.addEventListener("click", sortAdvice)
